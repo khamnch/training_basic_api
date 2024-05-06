3fe03f9b03d63dc5866c14037cb8cd8a74433919
khamnch <kham.nch@gmail.com>
2024-05-06 15:51:44 +0700

    update

--- varible.js
+++ varible.js
@@ -3,3 +3,31 @@ let pi = 3.14159;
 
 console.log(age);
 console.log(pi);
+
+//string type
+let first_name = "Mork";
+let last_name = "tssch";
+console.log(first_name + " " + last_name);
+
+//boolean type
+let isReading = true;
+let isSleeping = false;
+
+console.log(isReading);
+console.log(isSleeping);
+
+//null Type
+let car = null;
+let y;
+console.log(car);
+console.log(y);
+
+//symbol Type
+let sym2 = Symbol('fool');
+console.log(sym2);
+
+//object
+let myCar = new Object();
+myCar.make = "ford";
+myCar.model = "Mustang";
+console.log(myCar.make);
